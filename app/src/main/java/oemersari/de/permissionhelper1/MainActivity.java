25b221b9386e6e818874f0d5ee4598548498a747
oemersari <oemersari@gmail.com>
2019-11-28 19:14:41 +0100

    1

---
+++ app/src/main/java/oemersari/de/permissionhelper1/MainActivity.java
@@ -0,0 +1,24 @@
+package oemersari.de.permissionhelper1;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import oemersari.de.permissionhelper.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void OnClick(View v) {
+
+
+    }
+
+}
